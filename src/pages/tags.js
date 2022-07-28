eb570951a0ebb8cb69583c1238b6d8762bc89165
Claudiu Florin Popa <claudiup_15@yahoo.com>
2022-07-28 16:07:09 +0200

    Complete project

---
+++ src/pages/tags.js
@@ -0,0 +1,42 @@
+import React from "react"
+import Layout from "../components/Layout"
+import { graphql, Link } from "gatsby"
+import setupTags from "../utils/setupTags"
+import slugify from "slugify"
+import SEO from "../components/SEO"
+
+const Tags = ({ data }) => {
+  const newTags = setupTags(data.allContentfulRecipe.nodes)
+  return (
+    <Layout>
+      <SEO title="Tags" />
+      <main className="page">
+        <section className="tags-page">
+          {newTags.map((tag, index) => {
+            const [text, value] = tag
+            const slug = slugify(text, { lower: true })
+            return (
+              <Link key={index} to={`/tags/${slug}`} className="tag">
+                <h5>{text}</h5>
+                <p>{value}</p>
+              </Link>
+            )
+          })}
+        </section>
+      </main>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allContentfulRecipe {
+      nodes {
+        content {
+          tags
+        }
+      }
+    }
+  }
+`
+export default Tags
